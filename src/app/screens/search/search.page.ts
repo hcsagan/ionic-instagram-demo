46682b9724e8f6eb0208d22390ac786fe4a51766
Can <halil.sagan@wunder.is>
2021-01-11 19:47:06 +0300

    feat: search page implementation

--- src/app/screens/search/search.page.ts
+++ src/app/screens/search/search.page.ts
@@ -1,10 +1,18 @@
 import { Component } from "@angular/core";
-
+import faker from "faker";
 @Component({
   selector: "app-search",
   templateUrl: "search.page.html",
   styleUrls: ["search.page.scss"],
 })
 export class SearchPage {
+  posts: String[];
   constructor() {}
+  ngOnInit() {
+    let posts: String[] = [];
+    for (let i = 0; i <= 29; i++) {
+      posts.push(faker.image.image());
+    }
+    this.posts = posts;
+  }
 }

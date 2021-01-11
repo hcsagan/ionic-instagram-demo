79f9633235207faa4952d2199b2b98bc2f2c79a8
Can <halil.sagan@wunder.is>
2021-01-11 10:56:14 +0300

    feat: profile page added

--- src/app/screens/profile/profile.page.ts
+++ src/app/screens/profile/profile.page.ts
@@ -1,4 +1,15 @@
 import { Component } from "@angular/core";
+import faker from "faker";
+interface Profile {
+  userName: string;
+  name: string;
+  avatar: string;
+  description: string;
+  uri: string;
+  posts: number;
+  followers: number;
+  following: number;
+}
 
 @Component({
   selector: "app-profile",
@@ -6,5 +17,25 @@ import { Component } from "@angular/core";
   styleUrls: ["profile.page.scss"],
 })
 export class ProfilePage {
-  constructor() {}
+  constructor() {
+    this.profile = {
+      name: faker.name.findName(),
+      userName: faker.internet.userName(),
+      description: "Açıklama",
+      uri: "https://github.com",
+      avatar: "https://randomuser.me/api/portraits/men/92.jpg",
+      posts: Math.floor(Math.random() * 30),
+      followers: Math.floor(Math.random() * 300),
+      following: Math.floor(Math.random() * 300),
+    };
+  }
+  ngOnInit() {
+    let posts = [];
+    for (let i = 0; i < this.profile.posts; i++) {
+      posts.push(faker.image.image());
+    }
+    this.posts = posts;
+  }
+  posts: Object[] = [];
+  profile: Profile;
 }

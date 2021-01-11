7a108c5f446310ab8cf20f0b572995e1edabc626
Can <halil.sagan@wunder.is>
2021-01-11 10:56:01 +0300

    feat: home page

--- src/app/screens/home/home.page.ts
+++ src/app/screens/home/home.page.ts
@@ -1,10 +1,40 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import faker from "faker";
 
+function generatePost() {
+  const name = faker.internet.userName();
+  const avatar = faker.internet.avatar();
+  const image = faker.image.image();
+  return { name, avatar, image };
+}
 @Component({
   selector: "app-home",
   templateUrl: "home.page.html",
   styleUrls: ["home.page.scss"],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   constructor() {}
+  stories: Object[] | false = [];
+  posts: Object[] = [];
+
+  async ngOnInit() {
+    const users = await fetch(
+      "https://randomuser.me/api/?results=20&inc=name,picture&nat=en"
+    )
+      .then((res) => res.json())
+      .catch(() => false);
+
+    if (users) {
+      this.stories = users?.results;
+    } else {
+      this.stories = false;
+    }
+
+    let posts = [];
+    for (let i = 0; i < 20; i++) {
+      posts.push(generatePost());
+    }
+    this.posts = posts;
+    console.log(this.posts);
+  }
 }
